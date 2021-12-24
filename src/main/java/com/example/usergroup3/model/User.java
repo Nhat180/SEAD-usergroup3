bd07a0c88a48ff647c2ef3926a618b3d6ba23370
Nhat180 <s3740975@rmit.edu.vn>
2021-12-25 01:48:22 +0700

    Create User.java

---
+++ src/main/java/com/example/usergroup3/model/User.java
@@ -0,0 +1,31 @@
+package com.example.usergroup3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String address;
+
+    private String email;
+
+    private String phone;
+
+    private String role;
+}
+
