7a2f372d3119dd419e2366f4558ac777a3aba129
Nhat180 <s3740975@rmit.edu.vn>
2021-12-24 11:21:10 +0700

    First Commit

---
+++ src/main/java/com/example/usergroup3/UserGroup3Application.java
@@ -0,0 +1,13 @@
+package com.example.usergroup3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserGroup3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usergroup3/UserGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usergroup3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
